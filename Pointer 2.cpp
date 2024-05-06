582ae3971c6b8dd9a8540f0385c40ca44fba6e2d
Dhnnz12 <dhonanhibrizi123@gmail.com>
2024-05-06 15:00:21 +0700

    menambahkan class mahasiswa

--- Pointer 2.cpp
+++ Pointer 2.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+class mahasiswa
+{
+    public:
+    int nim;
+    void showNim()
+    {
+        cout << "No induk = " << nim << endl;
+    }
+};
+
+int main()
+{
+    mahs
+}
